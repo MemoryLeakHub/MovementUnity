c05ed822dfd44fd50b731dae682886f3b2d643d3
Vladislav Rashkov <mrcrozer@gmail.com>
2023-01-09 11:29:07 +0200

    initial

---
+++ Assets/Movement.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject redBox;
+    public GameObject blueBox;
+    public GameObject boxFacingLine;
+    
+    public TMP_Text timerText;
+    private float visualTime = 0.0f;
+    private float timer = 0.0f;
+    private bool start = false;
+    private int example = 0;
+    public void ResetPosition() {
+        HideFacingLine();
+        box.transform.rotation = Quaternion.identity;
+        start = false;
+        timer = 0;
+        visualTime = 0;
+        redBox.active = false;
+        timerText.gameObject.active = false;
+        box.transform.position = new Vector2(-5.72f, 1.12f);
+    }
+    public void Trigger() { 
+        start = true;
+    }
+    // Example 1 - transform.position
+    // Example 2 - transform.Translate 
+    // Example 3 = transform.position onUpdate
+    // Example 4 = transform.position onUpdate with Vector.right
+    // Example 5 = transform.position onUpdate with Vector.right show Destination 
+    // Example 6 = transform.position onUpdate with Vector.right show Destination when object is rotated
+    // Example 7 = transform.Translate onUpdate with Vector.right show Destination
+    // Example 8 = transform.Translate onUpdate with Vector.right show Destination when object is rotated
+    // Example 6 = transform.Translate onUpdate show Destination with directional Vector
+
+    // Directional Vectors
+    // Vector3.right     // (1,  0,  0)
+    // Vector3.left      // (-1, 0,  0)
+    // Vector3.up        // (0,  1,  0)
+    // Vector3.down      // (0, -1,  0)
+    // Vector3.forward   // (0,  0,  1)
+    // Vector3.back      // (0,  0, -1)
+
+    public void SetExample(int value) {
+        example = value;
+    }
+    // Example 1
+    // transform.position - set the position anywhere in the map
+    // move x position
+    // keep y position the same
+    public void SetXPosition(float x) { 
+        box.transform.position = new Vector3(x, box.transform.position.y);
+    }
+    // Example 2
+    // change the position based on the current box position
+    // add to the box x amount
+    public void TranslateX(float x) { 
+        box.transform.Translate(x,0,0);
+    }
+    void Update() { 
+        if (!start) {
+            return;
+        }
+        timer += Time.deltaTime;
+        if (example == 3) {
+            box.transform.position += new Vector3(2 * Time.deltaTime,0);   
+        }
+        if (example == 4) {
+            box.transform.position += Vector3.right * 2 * Time.deltaTime;    
+        }
+        if (example == 5) {
+            ShowRedBox(2);
+            if (box.transform.position.x >= redBox.transform.position.x) {
+                return;
+            }
+            box.transform.position += Vector3.right * 2 * Time.deltaTime;    
+            ShowTimer();
+        }
+        if (example == 6) {
+            ShowBoxFacingLine();
+            RotateBox(10);
+            ShowRedBox(2);
+            if (box.transform.position.x >= redBox.transform.position.x) {
+                return;
+            }
+            box.transform.position += Vector3.right * 2 * Time.deltaTime;  
+            ShowTimer();  
+        }
+        if (example == 7) {
+            ShowRedBox(2);
+            if (box.transform.position.x >= redBox.transform.position.x) {
+                return;
+            }
+            box.transform.Translate(Vector3.right * 2 * Time.deltaTime);
+            ShowTimer();
+        }
+        if (example == 8) {
+            ShowBoxFacingLine();
+            RotateBox(10);
+            ShowRedBox(2);
+            if (box.transform.position.x >= redBox.transform.position.x) {
+                return;
+            }
+            box.transform.Translate(Vector3.right * 2 * Time.deltaTime);
+            ShowTimer();
+        }
+    }
+
+    // Show Timer
+    private void ShowTimer() { 
+        timerText.gameObject.active = true;
+        visualTime = timer;
+        timerText.text = visualTime.ToString("f2");
+    }
+
+    // Show Destination
+    private void ShowRedBox(float x) { 
+        redBox.active = true;
+        redBox.transform.position = new Vector2(x, redBox.transform.position.y);
+    }
+    // Show Destination
+    private void ShowRedBox(float x, float y) { 
+        redBox.active = true;
+        redBox.transform.position = new Vector2(x, y);
+    }
+    // Rotate box
+    private void RotateBox(float angle) { 
+        box.transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+    }
+
+    private void ShowBoxFacingLine() { 
+        boxFacingLine.active = true;
+    }
+    
+    private void HideFacingLine() {  
+        boxFacingLine.active = false;
+    }
+}
---
+++ Assets/spritefont_and_other/document_and_picturelist/readme.txt
@@ -0,0 +1,26 @@
+
+Thanks for your download.
+
+
+This asset is a pixel art set of font and other pixel art set for retro type 2D games. 
+Each pixel art was drawn by only 4 colors.(sepia tone)  -like soft of old portable game-
+
+
+This asset contains three pictures.(in "pics" folder)
+
+fontsheet001.png------contains 10 types of sprite font.each size is 16x16 pixels and 8x8 pixels.
+fontsheet002.png------contains 7 types of sprite font.each size is 16x16 pixels and 8x8 pixels.
+other.png------contains some sprite for icons and for frame of window.
+
+
+If you want to choose purpose sprite quickly,refer the "picturelist.png","picturelist2.png" or"picturelist3.png"
+ (in "document_and_picturelist" folder). 
+
+
+
+<caution>
+Parency bug occurs in current version of unity when you use png image in [16bit] setting,so use png image in [true color] setting. 
+
+
+
+
